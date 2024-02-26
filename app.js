fd2a18f3f0a81398e69e2004dc1b388a65f831f3
havemersey <brenden@daymarker.digital>
2024-02-26 15:10:00 -0800

    App complete.

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-/* 
+/*
  * Package Imports
 */
-
+const session = require('express-session');
+const passport = require('passport');
 const path = require("path");
 require("dotenv").config();
 const express = require('express');
 const partials = require('express-partials');
-
+const GitHubStrategy = require('passport-github2').Strategy;
 
 const app = express();
-
 
 /*
  * Variable Declarations
 */
-
 const PORT = 3000;
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
@@ -22,35 +21,55 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 /*
  * Passport Configurations
 */
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/github/callback"
+  }, (accessToken, refreshToken, profile, done) => {
+    done(null, profile);
+  })
+);
 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
 
-
-
-
-
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
 /*
  *  Express Project Setup
 */
-
+app.use(session({
+  secret: 'codecademy',
+  resave: false,
+  saveUnitialized: false
+}));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(partials());
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
-
-
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 /*
  * Routes
 */
 
+app.get('/auth/github', passport.authenticate('github', { scope: ['user'] }));
+
+app.get('/auth/github/callback', passport.authenticate('github', {
+  failureRedirect: '/login',
+  successRedirect: '/'
+}))
+
 app.get('/', (req, res) => {
   res.render('index', { user: req.user });
 })
 
-app.get('/account', (req, res) => {
+app.get('/account', ensureAuthenticated, (req, res) => {
   res.render('account', { user: req.user });
 });
 
@@ -75,4 +94,11 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 /*
  * ensureAuthenticated Callback Function
 */
+function ensureAuthenticated(req, res, next) {
+  if ( req.isAuthenticated() ) {
+    return next();
+  } else {
+    res.redirect('/login');
+  }
+}
 
